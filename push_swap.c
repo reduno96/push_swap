2eb3392e8adebfa8ffaf0bd75fdcb417f044c172
reduno96 <reduno96@gmail.com>
2024-03-24 17:25:26 +0000

    Update code in add_nums_to_stack.c and push_swap.c

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rel-mora <reduno96@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 21:39:19 by rel-mora          #+#    #+#             */
-/*   Updated: 2024/03/24 17:18:21 by rel-mora         ###   ########.fr       */
+/*   Updated: 2024/03/24 17:24:06 by rel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(int argc, char **argv)
 	stack_a = NULL;
 	if (argc > 1)
 	{
-		add_nums_to_stack(&stack_a, argc, argv);
+		add_nums_to_stack(stack_a, argc, argv);
 		printf("stack: %d\n", stack_a->content);
 	}
 }
